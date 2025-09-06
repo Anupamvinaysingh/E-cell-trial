3a6bbe83026fbee6ce16123bbb76dc30b65c23bb
Anupamvinaysingh <anupambihat@gmail.com>
2025-09-06 20:29:39 +0530

    Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Aboutus } from './pages/home/aboutus/aboutus';
+import { Registration } from './pages/registration/registration';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },             // Default route
+  { path: 'about', component: Aboutus },       // Named route
+  { path: 'registration', component: Registration}
+];
+
